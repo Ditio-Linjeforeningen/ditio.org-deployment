ea12cf97c30b20e215286f441f6ba6b752524a57
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-10 17:50:30 +0100

    feat(dev shell): flake.nix with docker

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem = { pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            docker_28
+          ];
+        };
+      };
+    };
+}
